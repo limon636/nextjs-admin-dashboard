97c016bb15bb864c6c93ba270ba66c6b11b83836
Limon <limon.pstu@gmail.com>
2023-06-17 17:29:50 +0600

    Header and sidebar added

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,7 @@
 const HomePage = () => {
   return (
-    <div className="bg-green-500">
+    <div className="">
       <h1 className="text-6xl font-bold">Home Page</h1>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim
-        voluptatem deleniti delectus, assumenda maiores iste quia eum tenetur
-        explicabo nisi, nobis at dicta quaerat commodi ratione rerum.
-        Perspiciatis ullam porro aut exercitationem quaerat natus repellat totam
-        reprehenderit, fuga laboriosam optio voluptate debitis, labore atque
-        tempora officiis voluptas iste est maxime.
-      </p>
     </div>
   );
 };
